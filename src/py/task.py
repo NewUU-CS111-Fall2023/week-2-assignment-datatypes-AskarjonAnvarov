e73d386e83f436290e22ee7ada6f364ef9b57f48
AskarjonAnvarov <127713846+AskarjonAnvarov@users.noreply.github.com>
2023-10-25 23:36:31 +0500

    Update task.py

--- src/py/task.py
+++ src/py/task.py
@@ -1,13 +1,27 @@
-# * Author: Anvarov Askarjon
-# * Date: 25.10.2022
+from random import randint
 
-from tasks import NumberGuesser, LongArithmeticCalculator, StringMatcher
-print("Task_2")
-number = NumberGuesser()
-number.guess_number(int(input("Guess a number! \n")))
-print("Task_3")
-input_number = LongArithmeticCalculator()
-input_number.calculate_long_arithmetic(float(input("Enter a number! \n")))
-print("Task_4")
-string = StringMatcher()
-string.match_string(input("Write a sentence! \n").split(" "))
+
+class NumberGuesser:
+    def guess_number(self, target):
+        n = randint(1, 100)
+        while target != n:
+            if target > n:
+                print("Out of limit\n")
+                target = int(input("Plese predict again!\n"))
+            else:
+                print("Too low\n")
+                target = int(input("Please predict again!\n"))
+        print(f"You guessed the number {target}! Congratulations!!! ")
+
+class LongArithmeticCalculator:
+    def calculate_long_arithmetic(self, divisor):
+        x = randint(10**99, 10**100 - 1)
+        print(f"{x} is a random 100-digit number\n")
+        print(float(x / divisor))
+
+class StringMatcher:
+    def match_string(self, text):
+        if text[-1] in text[0:-1]:
+            print(f'Index of "{text[-1]}" in "{" ".join(text[0:-1])}" sentence is {text[0:-1].index(text[-1])}!')
+        else:
+            print("Does not match")
