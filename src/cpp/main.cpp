d58dfa868fb5ce75fbde1880c3808104e62ddde5
AskarjonAnvarov <127713846+AskarjonAnvarov@users.noreply.github.com>
2023-10-25 23:25:41 +0500

    Update main.cpp

--- src/cpp/main.cpp
+++ src/cpp/main.cpp
@@ -1,21 +1,30 @@
 /*
- * Author:
- * Date:
+ * Author: Anvarov Askarjon
+ * Date: 25.10.2023
  */
 
 #include <iostream>
 #include "task_1.h"
 
+class leapYear {
+public:
+    void leapYearValue(int year) {
+        if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+            std::cout << year << " is a leap year." << std::endl;
+        } else {
+            std::cout << year << " is not a leap year." << std::endl;
+        }
+    }
+};
+
+using namespace std;
 int main() {
-    std::cout << "Task 1" << std::endl;
-    // call for task 1
-    std::cout << "Task 2" << std::endl;
-    // call for task 2
-    std::cout << "Task 3" << std::endl;
-    // call for task 3
-    std::cout << "Task 4" << std::endl;
-    // call for task 4
-    std::cout << "Task 5" << std::endl;
-    // call for task 5
+    leapYear year_1;
+    cout<<"Task_1"<<endl;
+    cout<<"You can enter a year : "<<endl;
+    int x;
+    cin>>x;
+    year_1.leapYearValue(x);
+
     return 0;
 }
